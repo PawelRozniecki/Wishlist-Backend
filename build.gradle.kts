bd3845bbe22a5f599698ad4d3ecf1bdd62096665
Michał Regulski <regulskimichal@outlook.com>
2021-07-18 16:29:26 +0200

    Initial migration with Liquibase

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("org.springframework.boot") version "2.5.2"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.liquibase.gradle") version "2.0.3"
     java
 }
 
@@ -17,17 +18,45 @@ java {
     targetCompatibility = JavaVersion.VERSION_11
 }
 
+liquibase {
+    activities {
+        register("main") {
+            val dbRDBMS: String by project.extra.properties
+            val dbHost: String by project.extra.properties
+            val dbPort: String by project.extra.properties
+            val dbDatabase: String by project.extra.properties
+            val dbUsername: String by project.extra.properties
+            val dbPassword: String by project.extra.properties
+
+            arguments = mapOf(
+                "changeLogFile" to "src/main/db/db.yaml",
+                "url" to "jdbc:${dbRDBMS}://${dbHost}:${dbPort}/${dbDatabase}",
+                "username" to dbUsername,
+                "password" to dbPassword
+            )
+        }
+    }
+    runList = "main"
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-    compileOnly("org.projectlombok:lombok")
-    runtimeOnly("mysql:mysql-connector-java")
-    runtimeOnly("org.postgresql:postgresql")
     annotationProcessor("org.projectlombok:lombok")
+    compileOnly("org.projectlombok:lombok")
+    runtimeOnly("org.postgresql:postgresql")
+    liquibaseRuntime("org.liquibase:liquibase-core")
+    liquibaseRuntime("org.postgresql:postgresql")
+    liquibaseRuntime("org.yaml:snakeyaml")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.withType<ProcessResources> {
+    include("*.yml", "*.yaml", "*.properties")
+    expand(project.properties)
 }
